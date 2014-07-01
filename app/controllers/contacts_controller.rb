a33c3e9dbfcbebc96e11dfc30b07c65a67f637dd
Henry Huang <hhuang2088@gmail.com>
2014-07-01 15:12:17 -0700

    app now responds to json requests

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,5 +2,35 @@ class ContactsController < ApplicationController
 
   def main
   end
+
+  def index
+    @contacts = Contact.all
+
+    respond_to do |format|
+      format.json { render json: @contacts, only: [:name, :email, :number, :imgUrl]}
+    end
+  end
+
   
+  def new
+    @contacts = Contact.all
+    @contact = Contact.new 
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    respond_to do |f|
+      if @contact.save 
+        f.json { render json: @contact, status: :created}
+      else
+        f.json { render json: @contact.errors, status: :unprocessable_entity }
+    end
+  end
+
+    private 
+    def contact_params
+      params.require(:contact).permit(:name, :description, :phone, :img)
+    end
+  end
 end
